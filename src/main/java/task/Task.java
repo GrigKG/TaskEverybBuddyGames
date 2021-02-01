35b56c53a4cfa1e3821effc8c04d3cfa48db656a
GrigKG <38141067+GrigKG@users.noreply.github.com>
2021-02-01 17:11:04 +0200

    commit

--- src/main/java/task/Task.java
+++ src/main/java/task/Task.java
@@ -1,6 +1,7 @@
 package task;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -10,18 +11,19 @@ public class Task {
 	static ObjectMapper mapper = new ObjectMapper();
 	
 	@Autowired
-	Repository Repository;
+	Repository repository;
 
-	public void handle(String JSON) throws JsonMappingException, JsonProcessingException {
-		RequestDTO requestDTO = mapper.readValue(JSON, RequestDTO.class);	
-		if (!Repository.existsById(requestDTO.getRequestId())) {
-			String processRequest = processRequest(requestDTO.getRequestId(), requestDTO.getAttempt(),
-					requestDTO.getMethodName(), requestDTO.getArgs());
-			RestoreDTO restoreDTO = mapper.readValue(processRequest, RestoreDTO.class);
-			if ("Success".equals(restoreDTO.getStatus())) {
-				Repository.save(requestDTO);
-			}
+	private String SUCCESS = "Success";
+
+	public RestoreDTO handle(RequestDTO requestDTO) throws JsonMappingException, JsonProcessingException {
+		if (!repository.existsById(requestDTO.getRequestId())) throw new RuntimeException();
+		String processRequest = processRequest(requestDTO.getRequestId(), requestDTO.getAttempt(),
+				requestDTO.getMethodName(), requestDTO.getArgs());
+		RestoreDTO restoreDTO = mapper.readValue(processRequest, RestoreDTO.class);
+		if (SUCCESS.equals(restoreDTO.getStatus())) {
+			repository.save(requestDTO);
 		}
+		return restoreDTO;
 	}
 	
 	//given method
