5594d59bba18a4266c4cea86ae8091c748eafecd
GrigKG <38141067+GrigKG@users.noreply.github.com>
2021-02-01 17:12:33 +0200

    commit

--- src/main/java/task/Task.java
+++ src/main/java/task/Task.java
@@ -16,7 +16,7 @@ public class Task {
 	private String SUCCESS = "Success";
 
 	public RestoreDTO handle(RequestDTO requestDTO) throws JsonMappingException, JsonProcessingException {
-		if (!repository.existsById(requestDTO.getRequestId())) throw new RuntimeException();
+		if (repository.existsById(requestDTO.getRequestId())) throw new RuntimeException();
 		String processRequest = processRequest(requestDTO.getRequestId(), requestDTO.getAttempt(),
 				requestDTO.getMethodName(), requestDTO.getArgs());
 		RestoreDTO restoreDTO = mapper.readValue(processRequest, RestoreDTO.class);
